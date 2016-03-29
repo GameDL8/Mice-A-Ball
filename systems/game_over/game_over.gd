e457bb429fa948038ba175c86509814a83c904bd
BrunoSXS <brunosxs@gmail.com>
2016-03-29 19:55:10 -0300

    Finished trivia and cheats display system at credits

--- systems/game_over/game_over.gd
+++ systems/game_over/game_over.gd
@@ -1,9 +1,15 @@
 
 extends CanvasLayer
-
-var cheat_codes = ["Get +1 life:\n up,up,down,right,left","Enable debug mode:\nup,up,down,down,left,right,left,right","Look behind you!"]
-
+var trivia = ["Get +1 life:\n up,up,down,right,left","Enable debug mode:\nup,up,down,down,left,right,left,right","Look behind you!\n(No need for thanks :) )", "No Blue Rhinella schneideri were harmed during the production of this game.","Some programmers were harmed during the making of this game","It was planned for us to add an achievement system but time constrains made us scrap the idea.","Improve your memory by doing unforgettable things",""]
+var chances_modifier = 0
 func _ready():
+	set_process_input(true)
+	if GameManager.achievements["has_seen_credits"]:
+		chances_modifier = 2
+	if randi() %10 > 4-chances_modifier :
+		var selected_trivia = trivia[randi() % trivia.size()]
+		print(selected_trivia, " trivia will be displayed")
+		get_node("Credits/Credits").set_bbcode(get_node("Credits/Credits").get_bbcode()+"[center]" +"You're still here? Wow! We're flattered!\nAs thanks, we'd like to tell you something:\n"+selected_trivia )
 	get_node("Credits/Score").set_text("Your Score: "+str(GameManager.score))
 	restart()
 
@@ -12,3 +18,9 @@ func restart():
 	GameManager.achievements["has_seen_credits"] = true
 	GameManager.save_game()
 	GameManager.to_main_menu()
+	
+func _input(ev):
+	if ev.type == InputEvent.MOUSE_BUTTON and ev.button_index == 1 and ev.pressed:
+		get_node("AnimationPlayer").set_speed(10)
+	else:
+		get_node("AnimationPlayer").set_speed(1)
