367ec448eba4c86f20bd2b392bbcbc194662934b
BrunoSXS <brunosxs@gmail.com>
2016-03-30 17:43:56 -0300

    Chances are bigger now to see the mices at the intro

--- systems/intro/mice.gd
+++ systems/intro/mice.gd
@@ -11,5 +11,5 @@ func _ready():
 	pass
 
 func play():
-	if randi()%20 >14-modifier:
+	if randi()%20 >10-modifier:
 		anim.play(anim.get_animation_list()[randi() % anim.get_animation_list().size()])
