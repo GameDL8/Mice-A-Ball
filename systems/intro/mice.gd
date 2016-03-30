c9a23f2d6af1c0804b5b770c0d37c80ec9f28c71
BrunoSXS <brunosxs@gmail.com>
2016-03-30 17:37:32 -0300

    Removed useless files, added .gitignore

--- systems/intro/mice.gd
+++ systems/intro/mice.gd
@@ -1,12 +1,15 @@
 
 extends Sprite
 var anim
+var modifier = 0
 func _ready():
+	if GameManager.achievements["has_seen_credits"]:
+		modifier = 4
 	anim = get_node("anim")
 	# Called every time the node is added to the scene.
 	# Initialization here
 	pass
 
 func play():
-	if randi()%20 >14:
+	if randi()%20 >14-modifier:
 		anim.play(anim.get_animation_list()[randi() % anim.get_animation_list().size()])
